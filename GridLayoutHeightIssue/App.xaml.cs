30542a3a1a73638b3d865c06678f80f0caf32aad
NirmalKumarYuvaraj <97871636+NirmalKumarYuvaraj@users.noreply.github.com>
2023-08-25 16:59:45 +0530

    Add files via upload

---
+++ GridLayoutHeightIssue/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace GridLayoutHeightIssue
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ GridLayoutHeightIssue/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace GridLayoutHeightIssue
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
